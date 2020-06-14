a5d004c13aee2845d9ea3aece5519262dea5a4ff
Stephanie Hansen <Steph@Stephanies-MacBook-Pro.local>
2020-06-14 17:36:38 -0600

    objectfile

---
+++ pageObjects/checkpoint2Objects.js
@@ -0,0 +1,11 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        button1: 'button[name=evenOddButton]',
+        button2: 'button[name=objectFilterButton]',
+        button3: 'button[id=nameFilterButton]',
+        button4: 'button[name=palindromeButton]',
+        button5: 'button[name=sumButton]'
+        
+    } 
+}
